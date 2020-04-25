2550bae1d90ab5d57eb801368d3e7662a306fbc5
Sumit Govil <sumit.govil@optus.com.au>
2020-04-25 20:13:33 +1000

    initial setup

---
+++ src/client/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>I'm the the very best home component</div>;
+};
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Home from "./client/components/Home";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
