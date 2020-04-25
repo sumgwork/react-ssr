700aed377d3121357a3786e99756a092fa0bfd76
Sumit Govil <sumit.govil@optus.com.au>
2020-04-25 20:29:02 +1000

    inserted client side JS into the app

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div>I'm the the very best home component</div>;
+  return (
+    <div>
+      <div>I'm the the very best home component</div>
+      <button
+        onClick={() => {
+          console.log("Hi there");
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default Home;
---
+++ webpack.client.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/client.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+
+  // Run babel on every file
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "react",
+            "stage-0",
+            [
+              "env",
+              {
+                targets: {
+                  browsers: ["last 2 versions"],
+                },
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+};
